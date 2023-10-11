f267701c1779f92447ab1599f627eb7ccde1ef0e
tartegauri <134502992+tartegauri@users.noreply.github.com>
2023-10-12 00:10:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const userRouter = require("./router/userRouter.js");
 app.use(express.json());
 app.use(cors());
 app.use("/users",userRouter);
-
+app.get("/",(req,res)=>{
+  res.status(200).json({message:"Server Started"});
+})
 app.listen(3006,()=>{
   console.log("server started");
 })
